135f79e0491270b13744a1b212f8c99bbc95eccb
wilsonsantosnet <wilsonsantosnet@gmail.com>
2018-01-17 10:21:21 -0200

    ++

--- Common.API/Converters/DateTimePtBrModelBinderProvider .cs
+++ Common.API/Converters/DateTimePtBrModelBinderProvider .cs
@@ -27,10 +27,9 @@ namespace Common.API
         {
             var valueProviderResult = bindingContext.ValueProvider.GetValue(bindingContext.ModelName);
             var value = valueProviderResult.FirstValue;
-            DateTime outDate;
 
             //var parsed = DateTime.TryParse(value, CultureInfo.GetCultureInfo("pt-BR").DateTimeFormat, DateTimeStyles.None, out outDate);
-            var parsed = DateTime.TryParse(value, new CultureInfo("pt-BR").DateTimeFormat, DateTimeStyles.None, out outDate);
+            var parsed = DateTime.TryParse(value, new CultureInfo("pt-BR").DateTimeFormat, DateTimeStyles.None, out DateTime outDate);
 
             var result = ModelBindingResult.Success(outDate);
             if (!parsed)
