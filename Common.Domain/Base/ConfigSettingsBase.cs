9f92ec0939d4aff9549f9c32da6fa50753c9fa46
Wilson Santos <wilsonsantosnet@gmail.com>
2018-04-16 09:11:24 -0300

    add ApiEndPoint

--- Common.Domain/Base/ConfigSettingsBase.cs
+++ Common.Domain/Base/ConfigSettingsBase.cs
@@ -20,6 +20,8 @@ namespace Common.Domain.Base
         public string Salt { get; set; }
         
         public string FilePath { get; set; }
+        
+        public string ApiEndPoint { get; set; }
 
 
     }
